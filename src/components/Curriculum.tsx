2d3e738d4c224d1e252c2bee558cad2473aba89f
IANTSOGod <iantsochristianrazafindrazaka@gmail.com>
2025-09-06 18:53:11 +0300

    modif min

--- src/components/Curriculum.tsx
+++ src/components/Curriculum.tsx
@@ -32,7 +32,7 @@ const Curriculum = () => {
           className="text-center mb-16"
         >
           <h2 className="text-5xl font-dancing font-bold text-university-red mb-6">
-            Programmes par semestre de licence
+            Pour la licence, nous avons
           </h2>
           <p className="text-xl text-gray-700 max-w-3xl mx-auto font-montserrat leading-relaxed">
             Un cursus progressif sur 3 années pour acquérir toutes les
